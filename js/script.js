d590f679cf94b1d6d3d334df41e2b75635f31263
danielevergani <dvergani94@gmail.com>
2021-02-25 14:50:12 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+alert("con questo programma genereremo la tua password")
 
+document.getElementById("titolo").innerHTML = "GENERATORE PASSWORD";
+
+var nome = prompt("scrivi il tuo nome")
+document.getElementById("nome").innerHTML = "bene, il tuo nome è " + nome;
+
+var cognome = prompt("ora scrivi il tuo cognome");
+document.getElementById("cognome").innerHTML = " il tuo cognome è " + cognome;
+
+var colore = prompt("come ultima cosa scrivi il tuo colore preferito");
+document.getElementById("colore").innerHTML = " e il tuo colore preferito è il " + colore;
+
+document.getElementById("pw").innerHTML = "la tua password sarà " + nome + cognome + colore + "21";
